61e9819b85cc370fef9f59b20c3ec7b1faad58e0
Willem Van der Elst <28760191+n00bhax@users.noreply.github.com>
2021-04-18 16:46:59 +0200

    file sharing over tcp

---
+++ src/Client.java
@@ -0,0 +1,37 @@
+import java.io.*;
+import java.net.Socket;
+
+public class Client {
+    private static DataOutputStream dataOutputStream = null;
+    private static DataInputStream dataInputStream = null;
+
+    public static void main(String[] args) {
+        try(Socket socket = new Socket("localhost",5000)) {
+            dataInputStream = new DataInputStream(socket.getInputStream());
+            dataOutputStream = new DataOutputStream(socket.getOutputStream());
+
+            sendFile("C:\\Users\\Willem\\IdeaProjects\\6-DistributedSystems\\lab1\\src\\data.txt");
+
+            dataInputStream.close();
+            dataInputStream.close();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    private static void sendFile(String path) throws Exception{
+        int bytes;
+        File file = new File(path);
+        FileInputStream fileInputStream = new FileInputStream(file);
+
+        // send file size
+        dataOutputStream.writeLong(file.length());
+        // break file into chunks
+        byte[] buffer = new byte[4*1024];
+        while ((bytes=fileInputStream.read(buffer))!=-1){
+            dataOutputStream.write(buffer,0,bytes);
+            dataOutputStream.flush();
+        }
+        fileInputStream.close();
+    }
+}
